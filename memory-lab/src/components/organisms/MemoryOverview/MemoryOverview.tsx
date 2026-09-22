3ceb928d6ae47145bb408a0cbba570e5d9c15798
Kacper Kucal <kucalkacper27@gmail.com>
2026-09-22 10:48:43 +0200

    added stats display

--- memory-lab/src/components/organisms/MemoryOverview/MemoryOverview.tsx
+++ memory-lab/src/components/organisms/MemoryOverview/MemoryOverview.tsx
@@ -1,16 +1,20 @@
 
 import { StatItem } from '../../molecules/StatItem/StatItem'
 import { Heading } from '../../atoms/Heading/Heading'
+import { getStats } from '../../../utils/storage'
+import type { GameStats } from '../../../types/GameStats'
 
 export function MemoryOverview() {
+  const stats: GameStats = getStats();
+
   return (
     <section className="space-y-6">
       <div>
         <Heading>Statystyki</Heading>
         <div className="mt-4 grid gap-4 sm:grid-cols-3">
-          <StatItem label="Ukończone sesje" value="12" />
-          <StatItem label="Najlepsza seria" value="8 dni" />
-          <StatItem label="Średnia dokładność" value="86%" />
+          <StatItem label="Ilość gier" value={stats.gamesPlayed.toString()} />
+          <StatItem label="Ilość wygranych" value={stats.gamesWon.toString()} />
+          <StatItem label="Procent wygranych gier" value={`${Math.round(stats.gamesWon / stats.gamesPlayed * 100)}%`} />
         </div>
       </div>
     </section>
